2129c8cbab38955806aefde15aa4730c7fe189db
Angus <ngsmrk@gmail.com>
2018-05-12 18:26:15 +0100

    Added odd example

--- logistic_regression_example.rb
+++ logistic_regression_example.rb
@@ -3,12 +3,12 @@ require 'liblinear'
 # train
 model = Liblinear.train(
   { solver_type: Liblinear::L2R_LR },   # parameter
-  [-1, -1, 1, 1],                       # labels (classes) of training data
-  [[-2, -2], [-1, -1], [1, 1], [2, 2]], # training data
+  [-1, -1, 1, 1, 1],                       # labels (classes) of training data
+  [[-2, -2], [-1, -1], [1, 1], [2, 2], [-2, -2]], # training data
   -1, # bias
 )
 # predict
-(-1..1).step(0.1).each do | i |
+(-2..2).step(0.1).each do | i |
   rounded = i.round(1)
   p rounded
   puts "Prediction: #{Liblinear.predict(model, [rounded, rounded])}"
